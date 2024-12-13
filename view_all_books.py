9d21acb9eb02d8b29d4d616811b5331e6775bf4e
brikkho <brikkho.iba@gmail.com>
2024-12-13 21:17:34 +0600

    Update view_all_books.py

--- view_all_books.py
+++ view_all_books.py
@@ -14,5 +14,7 @@ def view_all_books(all_books):
     if all_books != []:
         for book in all_books:
             print(f"Title: {book['title']} | Author: {book['author']} | ISBN: {book['isbn']} | Year: {book['year']} | Price: {book['price']} | Quantity: {book['quantity']} | Book Added At: {book['bookAddedAt']} | Book Last Updated At: {book['bookLastUpdatedAt']}")
+            print(f"Total books in the Library as on todate: {len(all_books)}\n")
+            #print(f"Total books available for lending as on {toDate}: {len(all_books)}")
     else:
         print("No Book found.")
